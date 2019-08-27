09526f1c7fa963e847f503316cca4c333cbc553c
Stephen Blais <stephen@s-blais.com>
2019-08-27 16:54:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.collect do |holiday, supplies|
       holiday if supplies.include? ("BBQ")
-      end
     end
   end
 end
