46964cdea19f991884a3364d8c43c3064fad5ac8
Stephen Blais <stephen@s-blais.com>
2019-08-27 16:50:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
       if supplies.include? ("BBQ")
-        bbq_keys << holiday.key
+        bbq_keys << holiday
       end
     end
   end
